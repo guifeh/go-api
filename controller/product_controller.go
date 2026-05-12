a44389788e7955d432a8fecd66019e54e1976ed4
guifeh <guigasparinofernandes@gmail.com>
2026-05-11 22:01:15 -0300

    initial commit

---
+++ controller/product_controller.go
@@ -0,0 +1,27 @@
+package controller
+
+import (
+	"go-api/usecase"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type productController struct {
+	productUseCase usecase.ProductUseCase
+}
+
+func NewProductController(usecase usecase.ProductUseCase) productController {
+	return productController{
+		productUseCase: usecase,
+	}
+}
+
+func (p *productController) GetProducts(ctx *gin.Context) {
+	products, err := p.productUseCase.GetProducts()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, products)
+}
